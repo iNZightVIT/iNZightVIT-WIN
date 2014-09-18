e01d2ea1c18a766c43f55a3707375f42b3dfe3f8
Tom Elliott <tomelliott90@gmail.com>
2014-09-19 11:56:13 +1200

    updated the update script

--- prog_files/updateiNZightVIT.R
+++ prog_files/updateiNZightVIT.R
@@ -1,26 +1,9 @@
-# This is the update script for WINDOWS
+library(utils)
+if (!"RCurl" %in% rownames(installed.packages()))
+    install.packages("RCurl")
 library(RCurl)
 
-# First set up some global variables that will be used by the updater function
-Sys.setenv("R_HOME" = file.path(getwd(), "prog_files"))
-.libPaths(file.path(getwd(), "prog_files", "library"))
-isWindows <- TRUE
-isLinux <- FALSE
-isOSX <- FALSE
-OSstring <- "Windows"
-downloadMethod <- getOption("download.file.method", default = "auto")
-utils::setInternet2(TRUE)
-useOldR <- FALSE  # this can be set to TRUE for versions that need to
-                  # stick with an old version of R
-
-UPDATER_VERSION <- numeric_version(1.0)
-updater.location <- file.path(getwd(), "prog_files", "updateiNZightVIT.R")
-update.ext <- "-WIN"  # this will be "-WIN", "-MAC", "-MAC-snowleopard" etc...
-
 source_https <- function(url, ...) {
-  # load package
-    require(RCurl)
-
   # Download the new file to a temporary location and source it.
     sapply(c(url, ...), function(u) {
         text <- getURL(u, followlocation = TRUE,
@@ -32,6 +15,6 @@ source_https <- function(url, ...) {
         source(ftmp)
     })
 }
+
 source_https("https://www.stat.auckland.ac.nz/~wild/downloads/iNZight/update.R")
-#source(file.choose())  # this is for debugging
 updateDistribution()
