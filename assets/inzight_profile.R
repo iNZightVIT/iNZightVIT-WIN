6da58de446ceceec49d70614e6f792ae2aac6e5c
Tom Elliott <tomelliottnz@gmail.com>
2019-01-25 16:37:28 +1300

    dont use .Rprofile as the versioned name

---
+++ assets/inzight_profile.R
@@ -0,0 +1,60 @@
+## START iNZight
+
+# Things you might want to change
+
+# options(papersize="a4")
+# options(editor="notepad")
+# options(pager="internal")
+
+# set the default help type
+# options(help_type="text")
+options(help_type="html") ##, warn = -1)
+
+# set a site library
+# .Library.site <- file.path(chartr("\\", "/", R.home()), "site-library")
+
+
+# setting CRAN mirror to UoA
+local({
+  r <- getOption("repos")
+  r["CRAN"] <- "https://cloud.r-project.org"
+  options(repos = r)
+})
+
+# Sometimes we have the case where packages can't be found,
+# this could be due to R_HOME being in the wrong place (i.e.
+# current dir instead of proper R_HOME dir). To counteract this,
+# we set the R_HOME along with the libPath.
+Sys.setenv("R_HOME" = file.path(getwd(), "prog_files"))
+.libPaths(file.path(getwd(), "prog_files", "library"))
+
+grDevices::dev.new(width = 5, height = 2)
+grid::grid.newpage()
+# Will try to draw a raster if possible, otherwise an array of pixels
+
+try({
+  suppressWarnings({
+    splashImg <- png::readPNG(file.path(getwd(), "prog_files", "images", "inzight-banner.png"),
+                              exists("rasterImage"))
+  })
+  grid::grid.raster(splashImg)#, width = grid::unit(360, "points"), height = grid::unit(60, "points"))
+}, silent = TRUE)
+
+message("(Dept of Statistics, Uni. of Auckland)")
+message("")
+message("Please wait while iNZight loads...")
+
+suppressMessages(suppressWarnings({
+  library(iNZight)
+}))
+
+# Killing the splash screen, assigning to remove print
+tmp <- grDevices::dev.off()
+rm(tmp)
+
+# Let's go!
+suppressWarnings({
+  iNZight(disposeR = TRUE)
+})
+# the `disposeR = TRUE` makes sure when a user closes iNZight, they
+# close the entire R session (but not if they open it through R!)
--- bootstrap.R
+++ bootstrap.R
@@ -1,12 +1,15 @@
 ## read yaml config
 config <- yaml::read_yaml("pkg_versions.yml")
 
+if (dir.exists('prog_files')) unlink('prog_files', TRUE, TRUE)
+
 ## download R installer
 R_VERSION = config$r_version
 INST_FILE = sprintf("R-%s-win.exe", R_VERSION)
 LOCAL_DIR <- file.path(
     "~", ".wine", "drive_c", "Program Files", "R", sprintf("R-%s", R_VERSION)
 )
+# this will let us cache R?
 if (!dir.exists(LOCAL_DIR)) {
     dir.create(LOCAL_DIR, recursive = TRUE)
     inst.file <- file.path("https://cran.rstudio.com/bin/windows/base", INST_FILE)
@@ -29,61 +32,64 @@ for (dir in subdirs)
     x <- file.copy(file.path(LOCAL_DIR, dir), file.path("prog_files"), recursive = TRUE)
 x <- dir.create(file.path("prog_files", "images"))
 cat(" * copying assets\n")
+
 x <- file.copy(file.path("assets", "images"), file.path("prog_files"), recursive = TRUE)
-x <- file.copy(file.path("assets", "vit"), file.path("prog_files"), recursive = TRUE)
-x <- file.copy(file.path("assets", ".Rprofile"), file.path("prog_files"))
+x <- file.copy(file.path("assets", "inzight_profile.R"), file.path(".Rprofile"))
+dir.create(file.path("prog_files", "vit"))
+x <- file.copy(file.path("assets", "vit_profile.R"), file.path("prog_files", "vit", ".Rprofile"))
+x <- file.copy(file.path("assets", "update_profile.R"), file.path("prog_files", ".Rprofile"))
 
 
 ## run this to get all the necessary packages and get them updated and all that jazz
-cat(" * compiling list of required packages\n")
-pkglib <- file.path("prog_files", "library")
-pkgversions <- installed.packages(pkglib)[, 'Version']
+# cat(" * compiling list of required packages\n")
+# pkglib <- file.path("prog_files", "library")
+# pkgversions <- installed.packages(pkglib)[, 'Version']
 
-repos <- c('https://r.docker.stat.auckland.ac.nz', 'https://cran.stat.auckland.ac.nz')
-if (!requireNamespace('packrat', quietly = TRUE)) install.packages('packrat', repos = repos[2])
-if (!requireNamespace('devtools', quietly = TRUE)) install.packages('devtools', repos = repos[2])
+# repos <- c('https://r.docker.stat.auckland.ac.nz', 'https://cran.stat.auckland.ac.nz')
+# if (!requireNamespace('packrat', quietly = TRUE)) install.packages('packrat', repos = repos[2])
+# if (!requireNamespace('devtools', quietly = TRUE)) install.packages('devtools', repos = repos[2])
 
-ap <- available.packages(repos = repos)
-srclib <- .libPaths()[1]
-inzpkgs <- c('iNZight', 'iNZightPlots', 'iNZightModules', 'iNZightTools',
-             'iNZightRegression', 'iNZightMR', 'iNZightTS', 'vit')
-if (length(ca) > 0)
-    inzpkgs <- c(inzpkgs, ca)
+# ap <- available.packages(repos = repos)
+# srclib <- .libPaths()[1]
+# inzpkgs <- c('iNZight', 'iNZightPlots', 'iNZightModules', 'iNZightTools',
+#              'iNZightRegression', 'iNZightMR', 'iNZightTS', 'vit')
+# if (length(ca) > 0)
+#     inzpkgs <- c(inzpkgs, ca)
 
-extrapkgs <- packrat:::getPackageDependencies(inzpkgs, srclib, ap, 
-                                              fields = c('Depends', 'Imports', 'Suggests', 'LinkingTo'))
-if (!'iNZightMaps' %in% inzpkgs) {
-    extrapkgs <- extrapkgs[extrapkgs != "iNZightMaps"]
-    extrapkgs <- extrapkgs[extrapkgs != "sf"]
-}
-extrapkgs <- extrapkgs[extrapkgs != "Acinonyx"]
+# extrapkgs <- packrat:::getPackageDependencies(inzpkgs, srclib, ap, 
+#                                               fields = c('Depends', 'Imports', 'Suggests', 'LinkingTo'))
+# if (!'iNZightMaps' %in% inzpkgs) {
+#     extrapkgs <- extrapkgs[extrapkgs != "iNZightMaps"]
+#     extrapkgs <- extrapkgs[extrapkgs != "sf"]
+# }
+# extrapkgs <- extrapkgs[extrapkgs != "Acinonyx"]
 
-## Installing additional packages specified on command line ...
-deps <- unique(c(inzpkgs, extrapkgs, packrat:::recursivePackageDependencies(unique(c(inzpkgs, extrapkgs)), srclib, ap)))
-print(deps)
+# ## Installing additional packages specified on command line ...
+# deps <- unique(c(inzpkgs, extrapkgs, packrat:::recursivePackageDependencies(unique(c(inzpkgs, extrapkgs)), srclib, ap)))
+# print(deps)
 
-missing <- deps[!deps %in% names(pkgversions)]
+# missing <- deps[!deps %in% names(pkgversions)]
 
-pkgu <- pkgversions[names(pkgversions) %in% rownames(ap)]
-outdated <- names(pkgu)[ap[names(pkgu), 'Version'] > pkgu]
+# pkgu <- pkgversions[names(pkgversions) %in% rownames(ap)]
+# outdated <- names(pkgu)[ap[names(pkgu), 'Version'] > pkgu]
 
-grab <- unique(c(missing, outdated))
+# grab <- unique(c(missing, outdated))
 
-cat(" * downloading packages\n")
-pkgs <- download.packages(grab, pkglib, repos = repos, type = 'win.binary', quiet = TRUE)
+# cat(" * downloading packages\n")
+# pkgs <- download.packages(grab, pkglib, repos = repos, type = 'win.binary', quiet = TRUE)
 
-## unzip
-cat(" * extracting packages into place\n")
-x <- apply(pkgs, 1, function(pkg) {
-    pkgd <- file.path("prog_files", "library", pkg[1]) 
-    if (dir.exists(pkgd)) unlink(pkgd, TRUE, TRUE)
-    unzip(pkg[2], exdir = file.path("prog_files", "library"))
-    unlink(pkg[2])
-})
+# ## unzip
+# cat(" * extracting packages into place\n")
+# x <- apply(pkgs, 1, function(pkg) {
+#     pkgd <- file.path("prog_files", "library", pkg[1]) 
+#     if (dir.exists(pkgd)) unlink(pkgd, TRUE, TRUE)
+#     unzip(pkg[2], exdir = file.path("prog_files", "library"))
+#     unlink(pkg[2])
+# })
 
-## and stick gtk into place 
-cat(" * copying GTK library\n")
-x <- file.copy(file.path("assets", "gtk"), file.path("prog_files", "library", "RGtk2"), recursive = TRUE)
+# ## and stick gtk into place 
+# cat(" * copying GTK library\n")
+# x <- file.copy(file.path("assets", "gtk"), file.path("prog_files", "library", "RGtk2"), recursive = TRUE)
 
-## and then install the latest versions of things ...
-cat(" * Done!\n\nNow go to `dev` and install the development iNZight packages\nif this isn't the master release\n")
+# ## and then install the latest versions of things ...
+# cat(" * Done!\n\nNow go to `dev` and install the development iNZight packages\nif this isn't the master release\n")
