d24f6a316937db278aeceff8620590a98eab1acb
Tom Elliott <tomelliottnz@gmail.com>
2019-10-19 08:07:13 +1300

    All latest dev changes (19/10/19)

--- prog_files/library/htmlwidgets/doc/develop_advanced.R
+++ prog_files/library/htmlwidgets/doc/develop_advanced.R
@@ -24,7 +24,7 @@ htmlwidgets:::toJSON2(head(iris, 8), dataframe = 'values', pretty = TRUE)
 #          keep_vec_names = keep_vec_names, json_verbatim = TRUE,
 #          ...)
 #  }
-#  <bytecode: 0x7f88f5b8ff18>
+#  <bytecode: 0x7fe511839ae0>
 
 ## ----eval=FALSE----------------------------------------------------------
 #  fooWidget <- function(data, name, ...) {
